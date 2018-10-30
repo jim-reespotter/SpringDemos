56e148e86ec37aa5313a2ea72bcf96d0d70e76a8
Jim Potter <{ID}+{username}@users.noreply.github.com>
2018-10-30 21:47:14 +0000

    Added some static content and item details bit.

--- BasicSpringBootRestt/src/main/java/com/example/demo/RestServer.java
+++ BasicSpringBootRestt/src/main/java/com/example/demo/RestServer.java
@@ -1,23 +1,30 @@
 package com.example.demo;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Collection;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestServer {
 
-	private Set <ServedThing> things = new HashSet <ServedThing> ();
+	private Map <String, ServedThing> things = new HashMap <String, ServedThing> ();
 	
 	public RestServer () {
-		things.add(new ServedThing ("Thing1", "Value1"));
-		things.add(new ServedThing ("Thing2", "Value2"));		
+		things.put("Thing1", new ServedThing ("Thing1", "Value1"));
+		things.put("Thing2", new ServedThing ("Thing2", "Value2"));		
 	}
 	
-	@GetMapping ("/")
-	public Set <ServedThing> getContent () {
-		return things;
+	@GetMapping ("/things")
+	public Collection <ServedThing> getContent () {
+		return things.values();
+	}
+	
+	@GetMapping ("thing/{id}")
+	public ServedThing getThing (@PathVariable("id") String id) {
+		return things.get(id);
 	}
 }
